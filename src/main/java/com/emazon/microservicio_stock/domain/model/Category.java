ec63e9aa1bbe89c8e0e4d7aaedf7adc2a7e0772a
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-18 20:58:16 -0500

    feat: add category model

---
+++ src/main/java/com/emazon/microservicio_stock/domain/model/Category.java
@@ -0,0 +1,47 @@
+package com.emazon.microservicio_stock.domain.model;
+
+import com.emazon.microservicio_stock.domain.exception.EmptyFieldException;
+import com.emazon.microservicio_stock.domain.exception.MaxLengthException;
+import com.emazon.microservicio_stock.domain.util.DomainConstants;
+
+import static java.util.Objects.requireNonNull;
+
+public class Category {
+    private final Long idCategory;
+    private final String name;
+    private final String description;
+
+    public Category(Long idCategory, String name, String description) {
+        if (name.trim().isEmpty()) {
+            throw new EmptyFieldException(DomainConstants.Field.NAME.toString());
+        }
+
+        if (description.trim().isEmpty()) {
+            throw new EmptyFieldException(DomainConstants.Field.DESCRIPTION.toString());
+        }
+
+        if (name.trim().length() > 50) {
+            throw new MaxLengthException(DomainConstants.Field.NAME.toString());
+        }
+
+        if (description.trim().length() > 90) {
+            throw new MaxLengthException(DomainConstants.Field.DESCRIPTION.toString());
+        }
+
+        this.idCategory = idCategory;
+        this.name = requireNonNull(name, DomainConstants.FIELD_NAME_NULL_MESSAGE);
+        this.description = requireNonNull(description, DomainConstants.FIELD_DESCRIPTION_NULL_MESSAGE);
+    }
+
+    public Long getIdCategory() {
+        return idCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
