bfe60592fbb85122555c98b5427b896f61a3b063
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-19 21:21:23 -0500

    feat: add controller advice

---
+++ src/main/java/com/emazon/microservicio_stock/configuration/exceptionhandler/ControllerAdvisor.java
@@ -0,0 +1,38 @@
+package com.emazon.microservicio_stock.configuration.exceptionhandler;
+
+import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.exception.CategoryAlreadyExistsException;
+import com.emazon.microservicio_stock.configuration.Constants;
+import com.emazon.microservicio_stock.domain.exception.EmptyFieldException;
+import com.emazon.microservicio_stock.domain.exception.MaxLengthException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class ControllerAdvisor {
+    @ExceptionHandler(EmptyFieldException.class)
+    public ResponseEntity<ExceptionResponse> handleEmptyFieldException(EmptyFieldException exception) {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(
+                String.format(Constants.EMPTY_FIELD_EXCEPTION_MESSAGE, exception.getMessage()),
+                HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(MaxLengthException.class)
+    public ResponseEntity<ExceptionResponse> handleMaxLengthException(MaxLengthException exception) {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(
+                String.format(Constants.MAX_LENGTH_EXCEPTION_MESSAGE, exception.getMessage()),
+                HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(CategoryAlreadyExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleCategoryAlreadyExistsException(CategoryAlreadyExistsException exception) {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(
+                String.format(Constants.CATEGORY_ALREADY_EXISTS_EXCEPTION_MESSAGE, exception.getMessage()),
+                HttpStatus.BAD_REQUEST.toString(), LocalDateTime.now()));
+    }
+}
