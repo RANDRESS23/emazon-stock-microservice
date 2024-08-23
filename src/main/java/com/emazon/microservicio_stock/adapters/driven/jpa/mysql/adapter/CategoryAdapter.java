2a219cbdc32be2e7cf54c8405c83dd6127b9ae81
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-23 15:31:38 -0500

    feat: add new implementations methods to persistence port

--- src/main/java/com/emazon/microservicio_stock/adapters/driven/jpa/mysql/adapter/CategoryAdapter.java
+++ src/main/java/com/emazon/microservicio_stock/adapters/driven/jpa/mysql/adapter/CategoryAdapter.java
@@ -1,11 +1,18 @@
 package com.emazon.microservicio_stock.adapters.driven.jpa.mysql.adapter;
 
+import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.entity.CategoryEntity;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.exception.CategoryAlreadyExistsException;
+import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.exception.CategoryNotFoundException;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.mapper.ICategoryEntityMapper;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.repository.ICategoryRepository;
+import com.emazon.microservicio_stock.configuration.Constants;
 import com.emazon.microservicio_stock.domain.model.Category;
 import com.emazon.microservicio_stock.domain.spi.ICategoryPersistencePort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class CategoryAdapter implements ICategoryPersistencePort {
@@ -19,5 +26,24 @@ public class CategoryAdapter implements ICategoryPersistencePort {
         }
 
         categoryRepository.save(categoryEntityMapper.toEntity(category));
+    }
+
+    @Override
+    public void deleteCategory(String name) {
+        CategoryEntity categoryEntity = categoryRepository.findByName(name)
+                .orElseThrow(() -> new CategoryNotFoundException(Constants.CATEGORY_NOT_FOUND));
+        categoryRepository.delete(categoryEntity);
+    }
+
+    @Override
+    public Optional<Category> findByName(String name) {
+        return categoryRepository.findByName(name)
+                .map(categoryEntityMapper::toDomainModel);
+    }
+
+    @Override
+    public Page<Category> findAllCategories(Pageable pageable) {
+        return categoryRepository.findAll(pageable)
+                .map(categoryEntityMapper::toDomainModel);
     }
 }
