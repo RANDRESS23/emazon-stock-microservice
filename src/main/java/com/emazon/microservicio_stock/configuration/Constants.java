8ed53752c3310bc9ce1c136122ff25bdc2345d8b
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-23 11:13:19 -0500

    feat: add new configurations constants

--- src/main/java/com/emazon/microservicio_stock/configuration/Constants.java
+++ src/main/java/com/emazon/microservicio_stock/configuration/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String CATEGORY_ALREADY_EXISTS_EXCEPTION_MESSAGE = "The category you want to create already exists";
     public static final String EMPTY_FIELD_EXCEPTION_MESSAGE = "Field %s can not be empty";
     public static final String MAX_LENGTH_EXCEPTION_MESSAGE = "The %s field exceeded the maximum character limit";
+    public static final String CATEGORY_NOT_FOUND = "Category not found.";
 }
