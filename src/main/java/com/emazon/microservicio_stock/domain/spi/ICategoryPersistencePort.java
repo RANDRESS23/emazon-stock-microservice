2ae6cc09178a9834ca8ffda8874cb1f6472c5f7d
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-23 11:01:02 -0500

    feat: add new methods to persistence port

--- src/main/java/com/emazon/microservicio_stock/domain/spi/ICategoryPersistencePort.java
+++ src/main/java/com/emazon/microservicio_stock/domain/spi/ICategoryPersistencePort.java
@@ -1,7 +1,14 @@
 package com.emazon.microservicio_stock.domain.spi;
 
 import com.emazon.microservicio_stock.domain.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface ICategoryPersistencePort {
     void saveCategory(Category category);
+    void deleteCategory(String name);
+    Optional<Category> findByName(String name);
+    Page<Category> findAllCategories(Pageable pageable);
 }
