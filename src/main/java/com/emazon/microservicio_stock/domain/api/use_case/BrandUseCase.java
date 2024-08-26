171b7ebb6370350b3c32157f7df2a940496d5a69
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-26 10:27:47 -0500

    feat: add brand use case

---
+++ src/main/java/com/emazon/microservicio_stock/domain/api/use_case/BrandUseCase.java
@@ -0,0 +1,38 @@
+package com.emazon.microservicio_stock.domain.api.use_case;
+
+import com.emazon.microservicio_stock.domain.api.IBrandServicePort;
+import com.emazon.microservicio_stock.domain.exception.BrandNotFoundException;
+import com.emazon.microservicio_stock.domain.exception.InvalidBrandNameException;
+import com.emazon.microservicio_stock.domain.model.Brand;
+import com.emazon.microservicio_stock.domain.spi.IBrandPersistencePort;
+import com.emazon.microservicio_stock.domain.util.DomainConstants;
+
+public class BrandUseCase implements IBrandServicePort {
+    private final IBrandPersistencePort brandPersistencePort;
+
+    public BrandUseCase(IBrandPersistencePort brandPersistencePort) {
+        this.brandPersistencePort = brandPersistencePort;
+    }
+
+    @Override
+    public void saveBrand(Brand brand) {
+        if(brandPersistencePort.getBrand(brand.getName()).isPresent()) {
+            throw new InvalidBrandNameException(DomainConstants.BRAND_ALREADY_EXISTS_MESSAGE);
+        }
+
+        brandPersistencePort.saveBrand(brand);
+    }
+
+    @Override
+    public void deleteBrand(String name) {
+        brandPersistencePort.getBrand(name)
+                .orElseThrow(() -> new BrandNotFoundException(DomainConstants.BRAND_NOT_FOUND));
+        brandPersistencePort.deleteBrand(name);
+    }
+
+    @Override
+    public Brand getBrand(String name) {
+        return brandPersistencePort.getBrand(name)
+                .orElseThrow(() -> new BrandNotFoundException(DomainConstants.BRAND_NOT_FOUND));
+    }
+}
