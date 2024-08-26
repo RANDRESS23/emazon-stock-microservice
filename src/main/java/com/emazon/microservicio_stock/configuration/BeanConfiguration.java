c1e2d35613560ce36b76c8773925e4f76299c4ab
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-26 12:00:24 -0500

    feat: add new beans configuration

--- src/main/java/com/emazon/microservicio_stock/configuration/BeanConfiguration.java
+++ src/main/java/com/emazon/microservicio_stock/configuration/BeanConfiguration.java
@@ -1,10 +1,16 @@
 package com.emazon.microservicio_stock.configuration;
 
+import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.adapter.BrandAdapter;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.adapter.CategoryAdapter;
+import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.mapper.IBrandEntityMapper;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.mapper.ICategoryEntityMapper;
+import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.repository.IBrandRepository;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.repository.ICategoryRepository;
+import com.emazon.microservicio_stock.domain.api.IBrandServicePort;
 import com.emazon.microservicio_stock.domain.api.ICategoryServicePort;
+import com.emazon.microservicio_stock.domain.api.use_case.BrandUseCase;
 import com.emazon.microservicio_stock.domain.api.use_case.CategoryUseCase;
+import com.emazon.microservicio_stock.domain.spi.IBrandPersistencePort;
 import com.emazon.microservicio_stock.domain.spi.ICategoryPersistencePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +21,8 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
     private final ICategoryRepository categoryRepository;
     private final ICategoryEntityMapper categoryEntityMapper;
+    private final IBrandRepository brandRepository;
+    private final IBrandEntityMapper brandEntityMapper;
 
     @Bean
     public ICategoryPersistencePort categoryPersistencePort() {
@@ -24,5 +32,15 @@ public class BeanConfiguration {
     @Bean
     public ICategoryServicePort categoryServicePort() {
         return new CategoryUseCase(categoryPersistencePort());
+    }
+
+    @Bean
+    public IBrandPersistencePort brandPersistencePort() {
+        return new BrandAdapter(brandRepository, brandEntityMapper);
+    }
+
+    @Bean
+    public IBrandServicePort brandServicePort() {
+        return new BrandUseCase(brandPersistencePort());
     }
 }
