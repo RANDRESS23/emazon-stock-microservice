648a39428fbce1c3a434c3dcd6b715a571855d32
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-26 11:38:33 -0500

    feat: add new constants

--- src/main/java/com/emazon/microservicio_stock/configuration/Constants.java
+++ src/main/java/com/emazon/microservicio_stock/configuration/Constants.java
@@ -6,7 +6,9 @@ public class Constants {
     }
 
     public static final String CATEGORY_ALREADY_EXISTS_EXCEPTION_MESSAGE = "The category you want to create already exists";
+    public static final String BRAND_ALREADY_EXISTS_EXCEPTION_MESSAGE = "The brand you want to create already exists";
     public static final String EMPTY_FIELD_EXCEPTION_MESSAGE = "Field %s can not be empty";
     public static final String MAX_LENGTH_EXCEPTION_MESSAGE = "The %s field exceeded the maximum character limit";
     public static final String CATEGORY_NOT_FOUND = "Category not found.";
+    public static final String BRAND_NOT_FOUND = "Brand not found.";
 }
