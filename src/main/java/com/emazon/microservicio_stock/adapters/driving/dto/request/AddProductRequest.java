bef56e19b7363451fb2db6a8578bb92e843908aa
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-30 08:39:43 -0500

    feat: add product request and response dto

---
+++ src/main/java/com/emazon/microservicio_stock/adapters/driving/dto/request/AddProductRequest.java
@@ -0,0 +1,18 @@
+package com.emazon.microservicio_stock.adapters.driving.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class AddProductRequest {
+    private final String name;
+    private final String description;
+    private final Long quantity;
+    private final BigDecimal price;
+    private List<Long> idCategories;
+    private Long idBrand;
+}
