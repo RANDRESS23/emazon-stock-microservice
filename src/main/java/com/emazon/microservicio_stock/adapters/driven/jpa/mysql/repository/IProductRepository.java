7d0527a2e3d8dda73bbe623a2934e865463393c1
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-30 08:40:29 -0500

    feat: add product repository

---
+++ src/main/java/com/emazon/microservicio_stock/adapters/driven/jpa/mysql/repository/IProductRepository.java
@@ -0,0 +1,10 @@
+package com.emazon.microservicio_stock.adapters.driven.jpa.mysql.repository;
+
+import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IProductRepository extends JpaRepository<ProductEntity, Long> {
+    Optional<ProductEntity> findByName(String name);
+}
