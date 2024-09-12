f752222d713bed33d815446827fa2d460762f7b0
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-12 15:07:29 -0500

    refactor: change some exceptions

--- src/main/java/com/emazon/microservicio_stock/domain/api/use_case/BrandUseCase.java
+++ src/main/java/com/emazon/microservicio_stock/domain/api/use_case/BrandUseCase.java
@@ -1,11 +1,12 @@
 package com.emazon.microservicio_stock.domain.api.use_case;
 
 import com.emazon.microservicio_stock.domain.api.IBrandServicePort;
-import com.emazon.microservicio_stock.domain.exception.BrandNotFoundException;
-import com.emazon.microservicio_stock.domain.exception.InvalidBrandNameException;
+import com.emazon.microservicio_stock.domain.exception.AlreadyExistsFieldException;
+import com.emazon.microservicio_stock.domain.exception.NotFoundException;
 import com.emazon.microservicio_stock.domain.model.Brand;
 import com.emazon.microservicio_stock.domain.spi.IBrandPersistencePort;
 import com.emazon.microservicio_stock.domain.util.DomainConstants;
+import com.emazon.microservicio_stock.domain.validation.BrandValidation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,36 +14,39 @@ import org.springframework.data.domain.Sort;
 
 public class BrandUseCase implements IBrandServicePort {
     private final IBrandPersistencePort brandPersistencePort;
+    private final BrandValidation brandValidation;
 
-    public BrandUseCase(IBrandPersistencePort brandPersistencePort) {
+    public BrandUseCase(IBrandPersistencePort brandPersistencePort, BrandValidation brandValidation) {
         this.brandPersistencePort = brandPersistencePort;
+        this.brandValidation = brandValidation;
     }
 
     @Override
     public void saveBrand(Brand brand) {
         if(brandPersistencePort.getBrand(brand.getName()).isPresent()) {
-            throw new InvalidBrandNameException(DomainConstants.BRAND_ALREADY_EXISTS_MESSAGE);
+            throw new AlreadyExistsFieldException(DomainConstants.BRAND_ALREADY_EXISTS_MESSAGE);
         }
 
+        brandValidation.validateBrand(brand);
         brandPersistencePort.saveBrand(brand);
     }
 
     @Override
     public void deleteBrand(String name) {
         Brand brand = brandPersistencePort.getBrand(name)
-                .orElseThrow(() -> new BrandNotFoundException(DomainConstants.BRAND_NOT_FOUND));
+                .orElseThrow(() -> new NotFoundException(DomainConstants.BRAND_NOT_FOUND));
         brandPersistencePort.deleteBrand(brand.getName());
     }
 
     @Override
     public Brand getBrand(String name) {
         return brandPersistencePort.getBrand(name)
-                .orElseThrow(() -> new BrandNotFoundException(DomainConstants.BRAND_NOT_FOUND));
+                .orElseThrow(() -> new NotFoundException(DomainConstants.BRAND_NOT_FOUND));
     }
 
     @Override
     public Page<Brand> getAllBrands(Integer page, Integer size, Boolean ascending) {
-        Sort sort = Boolean.TRUE.equals(ascending) ? Sort.by("name").ascending() : Sort.by("name").descending();
+        Sort sort = Boolean.TRUE.equals(ascending) ? Sort.by(DomainConstants.FIELD_NAME).ascending() : Sort.by(DomainConstants.FIELD_NAME).descending();
         Pageable pageable = PageRequest.of(page, size, sort);
 
         return brandPersistencePort.getAllBrands(pageable);
