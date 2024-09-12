40accb17f2e48e9a7ee0a2ddc9acb794392b299e
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-12 15:03:48 -0500

    refactor: remove unnecessary validations in the models

--- src/main/java/com/emazon/microservicio_stock/domain/model/Category.java
+++ src/main/java/com/emazon/microservicio_stock/domain/model/Category.java
@@ -1,40 +1,22 @@
 package com.emazon.microservicio_stock.domain.model;
 
-import com.emazon.microservicio_stock.domain.exception.EmptyFieldException;
-import com.emazon.microservicio_stock.domain.exception.MaxLengthException;
 import com.emazon.microservicio_stock.domain.util.DomainConstants;
 
 import static java.util.Objects.requireNonNull;
 
 public class Category {
-    private final Long idCategory;
+    private final Long categoryId;
     private final String name;
     private final String description;
 
-    public Category(Long idCategory, String name, String description) {
-        if (name.trim().isEmpty()) {
-            throw new EmptyFieldException(DomainConstants.Field.NAME.toString());
-        }
-
-        if (description.trim().isEmpty()) {
-            throw new EmptyFieldException(DomainConstants.Field.DESCRIPTION.toString());
-        }
-
-        if (name.trim().length() > DomainConstants.MAXIMUM_NAME_CHARACTERS) {
-            throw new MaxLengthException(DomainConstants.Field.NAME.toString());
-        }
-
-        if (description.trim().length() > DomainConstants.MAXIMUM_DESCRIPTION_CHARACTERS_CATEGORY) {
-            throw new MaxLengthException(DomainConstants.Field.DESCRIPTION.toString());
-        }
-
-        this.idCategory = idCategory;
+    public Category(Long categoryId, String name, String description) {
+        this.categoryId = categoryId;
         this.name = requireNonNull(name, DomainConstants.FIELD_NAME_NULL_MESSAGE);
         this.description = requireNonNull(description, DomainConstants.FIELD_DESCRIPTION_NULL_MESSAGE);
     }
 
-    public Long getIdCategory() {
-        return idCategory;
+    public Long getCategoryId() {
+        return categoryId;
     }
 
     public String getName() {
