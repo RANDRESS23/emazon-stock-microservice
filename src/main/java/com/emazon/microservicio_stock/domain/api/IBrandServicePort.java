a85d4146bac42e8b28f7628faa9e8a98dac17675
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-15 19:41:05 -0500

    refactor: change some details in the category and brand ports

--- src/main/java/com/emazon/microservicio_stock/domain/api/IBrandServicePort.java
+++ src/main/java/com/emazon/microservicio_stock/domain/api/IBrandServicePort.java
@@ -4,7 +4,7 @@ import com.emazon.microservicio_stock.domain.model.Brand;
 import org.springframework.data.domain.Page;
 
 public interface IBrandServicePort {
-    void saveBrand(Brand brand);
+    Brand saveBrand(Brand brand);
     void deleteBrand(String name);
     Brand getBrand(String name);
     Page<Brand> getAllBrands(Integer page, Integer size, Boolean ascending);
