2970bbcbc88098b3e8f344ead250a02e6ac27e77
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-25 15:13:04 -0500

    feat: add new product tests

--- src/test/java/com/emazon/microservicio_stock/adapters/driven/jpa/mysql/adapter/ProductAdapterTest.java
+++ src/test/java/com/emazon/microservicio_stock/adapters/driven/jpa/mysql/adapter/ProductAdapterTest.java
@@ -1,22 +1,23 @@
 package com.emazon.microservicio_stock.adapters.driven.jpa.mysql.adapter;
 
-import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.entity.BrandEntity;
-import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.entity.CategoryEntity;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.entity.ProductEntity;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.exception.AlreadyExistsException;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.mapper.IProductEntityMapper;
 import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.repository.IProductRepository;
+import com.emazon.microservicio_stock.adapters.driven.jpa.mysql.util.DrivenConstants;
 import com.emazon.microservicio_stock.domain.model.Brand;
 import com.emazon.microservicio_stock.domain.model.Category;
+import com.emazon.microservicio_stock.domain.model.CustomPage;
 import com.emazon.microservicio_stock.domain.model.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,53 +37,198 @@ class ProductAdapterTest {
     @InjectMocks
     private ProductAdapter productAdapter;
 
+    private ProductEntity productEntity1;
+    private ProductEntity productEntity2;
+    private Product product1;
+    private Product product2;
+
+    @BeforeEach
+    void setUp() {
+        Category category = new Category(1L, "Electronics", "Category for electronics");
+        Brand brand = new Brand(1L, "Brand A", "Brand Description");
+
+        productEntity1 = new ProductEntity();
+        productEntity1.setProductId(1L);
+        productEntity1.setName("Product 1");
+        productEntity1.setDescription("Description 1");
+        productEntity1.setQuantity(10L);
+        productEntity1.setPrice(BigDecimal.valueOf(100.0));
+
+        productEntity2 = new ProductEntity();
+        productEntity2.setProductId(2L);
+        productEntity2.setName("Product 2");
+        productEntity2.setDescription("Description 2");
+        productEntity2.setQuantity(20L);
+        productEntity2.setPrice(BigDecimal.valueOf(200.0));
+
+        product1 = new Product(1L, "Product 1", "Description 1", 10L, BigDecimal.valueOf(100.0), List.of(category), brand);
+        product2 = new Product(2L, "Product 2", "Description 2", 20L, BigDecimal.valueOf(200.0), List.of(category), brand);
+    }
+
     @Test
     void saveProduct() {
-        Category category = new Category(1L, "Electronics", "Electronics");
-        Brand brand = new Brand(1L, "Electronics", "Electronics");
-        List<Category> categories = new ArrayList<>();
-        categories.add(category);
-
-        CategoryEntity categoryEntity = new CategoryEntity(1L, "Electronics", "Electronics");
-        BrandEntity brandEntity = new BrandEntity(1L, "Electronics", "Electronics");
-        List<CategoryEntity> categoriesEntity = new ArrayList<>();
-        categoriesEntity.add(categoryEntity);
-
-        Product product = new Product(1L, "Electronics", "Electronics", 10L, new BigDecimal(25.5), categories, brand);
-        ProductEntity productEntity = new ProductEntity(1L, "Electronics", "Electronics", 10L, new BigDecimal(25.5), categoriesEntity, brandEntity);
-
-        when(productRepository.findByName(product.getName())).thenReturn(Optional.empty());
-        when(productEntityMapper.toEntity(product)).thenReturn(productEntity);
-        when(productRepository.save(any(ProductEntity.class))).thenReturn(productEntity);
+        when(productRepository.findByName(product1.getName())).thenReturn(Optional.empty());
+        when(productEntityMapper.toEntity(product1)).thenReturn(productEntity1);
+        when(productRepository.save(any(ProductEntity.class))).thenReturn(productEntity1);
 
         // Verificamos que no se lanza ninguna excepción al guardar el producto
-        assertDoesNotThrow(() -> productAdapter.saveProduct(product));
+        assertDoesNotThrow(() -> productAdapter.saveProduct(product1));
 
         // Verificamos que el método save fue llamado una vez
-        verify(productRepository, times(1)).save(productEntity);
+        verify(productRepository, times(1)).save(productEntity1);
     }
 
     @Test
     void saveProductWhenAlreadyExists() {
-        Category category = new Category(1L, "Electronics", "Electronics");
-        Brand brand = new Brand(1L, "Electronics", "Electronics");
-        List<Category> categories = new ArrayList<>();
-        categories.add(category);
-
-        CategoryEntity categoryEntity = new CategoryEntity(1L, "Electronics", "Electronics");
-        BrandEntity brandEntity = new BrandEntity(1L, "Electronics", "Electronics");
-        List<CategoryEntity> categoriesEntity = new ArrayList<>();
-        categoriesEntity.add(categoryEntity);
-
-        Product product = new Product(1L, "Electronics", "Electronics", 10L, new BigDecimal(25.5), categories, brand);
-        ProductEntity productEntity = new ProductEntity(1L, "Electronics", "Electronics", 10L, new BigDecimal(25.5), categoriesEntity, brandEntity);
-
-        when(productRepository.findByName(product.getName())).thenReturn(Optional.of(productEntity));
+        when(productRepository.findByName(product1.getName())).thenReturn(Optional.of(productEntity1));
 
         // Verificamos que se lanza una excepción cuando el producto ya existe
-        assertThrows(AlreadyExistsException.class, () -> productAdapter.saveProduct(product));
+        assertThrows(AlreadyExistsException.class, () -> productAdapter.saveProduct(product1));
 
         // Verificamos que el método save nunca fue llamado
         verify(productRepository, never()).save(any(ProductEntity.class));
+    }
+
+    @Test
+    void testGetAllProductsSortByProductNameAscending() {
+        // Arrange
+        int page = 0;
+        int size = 10;
+        boolean ascending = true;
+        String sortBy = DrivenConstants.SORT_BY_PRODUCT_NAME;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(DrivenConstants.SORT_BY_PRODUCT_NAME).ascending());
+
+        List<ProductEntity> productEntities = List.of(productEntity1, productEntity2);
+        Page<ProductEntity> pageProductEntities = new PageImpl<>(productEntities, pageable, productEntities.size());
+
+        when(productRepository.findAll(pageable)).thenReturn(pageProductEntities);
+        when(productEntityMapper.toPageOfProducts(pageProductEntities)).thenReturn(new PageImpl<>(List.of(product1, product2), pageable, productEntities.size()));
+
+        // Act
+        CustomPage<Product> result = productAdapter.getAllProducts(page, size, ascending, sortBy);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        assertEquals(1, result.getTotalPages());
+        assertEquals(page, result.getPageNumber());
+        assertEquals(size, result.getPageSize());
+        assertEquals(2, result.getContent().size());
+
+        Product firstProduct = result.getContent().get(0);
+        assertEquals(1L, firstProduct.getProductId());
+        assertEquals("Product 1", firstProduct.getName());
+
+        Product secondProduct = result.getContent().get(1);
+        assertEquals(2L, secondProduct.getProductId());
+        assertEquals("Product 2", secondProduct.getName());
+
+        verify(productRepository).findAll(pageable);
+        verify(productEntityMapper).toPageOfProducts(pageProductEntities);
+    }
+
+    @Test
+    void testGetAllProductsSortByBrandDescending() {
+        // Arrange
+        int page = 0;
+        int size = 10;
+        boolean ascending = false;
+        String sortBy = DrivenConstants.FIELD_BRAND;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(DrivenConstants.SORT_BY_BRAND_NAME).descending());
+
+        List<ProductEntity> productEntities = List.of(productEntity1, productEntity2);
+        Page<ProductEntity> pageProductEntities = new PageImpl<>(productEntities, pageable, productEntities.size());
+
+        when(productRepository.findAll(pageable)).thenReturn(pageProductEntities);
+        when(productEntityMapper.toPageOfProducts(pageProductEntities)).thenReturn(new PageImpl<>(List.of(product1, product2), pageable, productEntities.size()));
+
+        // Act
+        CustomPage<Product> result = productAdapter.getAllProducts(page, size, ascending, sortBy);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        assertEquals(1, result.getTotalPages());
+        assertEquals(page, result.getPageNumber());
+        assertEquals(size, result.getPageSize());
+        assertEquals(2, result.getContent().size());
+
+        Product firstProduct = result.getContent().get(0);
+        assertEquals(1L, firstProduct.getProductId());
+        assertEquals("Product 1", firstProduct.getName());
+
+        Product secondProduct = result.getContent().get(1);
+        assertEquals(2L, secondProduct.getProductId());
+        assertEquals("Product 2", secondProduct.getName());
+
+        verify(productRepository).findAll(pageable);
+        verify(productEntityMapper).toPageOfProducts(pageProductEntities);
+    }
+
+    @Test
+    void testGetAllProductsSortByCategoryAscending() {
+        // Arrange
+        int page = 0;
+        int size = 10;
+        boolean ascending = true;
+        String sortBy = DrivenConstants.FIELD_CATEGORIES;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(DrivenConstants.SORT_BY_CATEGORY_NAME).ascending());
+
+        List<ProductEntity> productEntities = List.of(productEntity1, productEntity2);
+        Page<ProductEntity> pageProductEntities = new PageImpl<>(productEntities, pageable, productEntities.size());
+
+        when(productRepository.findAll(pageable)).thenReturn(pageProductEntities);
+        when(productEntityMapper.toPageOfProducts(pageProductEntities)).thenReturn(new PageImpl<>(List.of(product1, product2), pageable, productEntities.size()));
+
+        // Act
+        CustomPage<Product> result = productAdapter.getAllProducts(page, size, ascending, sortBy);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        assertEquals(1, result.getTotalPages());
+        assertEquals(page, result.getPageNumber());
+        assertEquals(size, result.getPageSize());
+        assertEquals(2, result.getContent().size());
+
+        Product firstProduct = result.getContent().get(0);
+        assertEquals(1L, firstProduct.getProductId());
+        assertEquals("Product 1", firstProduct.getName());
+
+        Product secondProduct = result.getContent().get(1);
+        assertEquals(2L, secondProduct.getProductId());
+        assertEquals("Product 2", secondProduct.getName());
+
+        verify(productRepository).findAll(pageable);
+        verify(productEntityMapper).toPageOfProducts(pageProductEntities);
+    }
+
+    @Test
+    void testGetAllProductsWithEmptyPage() {
+        // Arrange
+        int page = 0;
+        int size = 10;
+        boolean ascending = true;
+        String sortBy = DrivenConstants.SORT_BY_PRODUCT_NAME;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(DrivenConstants.SORT_BY_PRODUCT_NAME).ascending());
+
+        Page<ProductEntity> pageProductEntities = new PageImpl<>(List.of(), pageable, 0);
+
+        when(productRepository.findAll(pageable)).thenReturn(pageProductEntities);
+        when(productEntityMapper.toPageOfProducts(pageProductEntities)).thenReturn(new PageImpl<>(List.of(), pageable, 0));
+
+        // Act
+        CustomPage<Product> result = productAdapter.getAllProducts(page, size, ascending, sortBy);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(0, result.getTotalElements());
+        assertEquals(0, result.getTotalPages());
+        assertEquals(page, result.getPageNumber());
+        assertEquals(size, result.getPageSize());
+        assertTrue(result.getContent().isEmpty());
+
+        verify(productRepository).findAll(pageable);
+        verify(productEntityMapper).toPageOfProducts(pageProductEntities);
     }
 }
--- src/test/java/com/emazon/microservicio_stock/adapters/driving/controller/ProductRestControllerTest.java
+++ src/test/java/com/emazon/microservicio_stock/adapters/driving/controller/ProductRestControllerTest.java
@@ -10,14 +10,13 @@ import com.emazon.microservicio_stock.adapters.driving.mapper.IProductResponseMa
 import com.emazon.microservicio_stock.domain.api.IProductServicePort;
 import com.emazon.microservicio_stock.domain.model.Brand;
 import com.emazon.microservicio_stock.domain.model.Category;
+import com.emazon.microservicio_stock.domain.model.CustomPage;
 import com.emazon.microservicio_stock.domain.model.Product;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -93,36 +92,6 @@ class ProductRestControllerTest {
     }
 
     @Test
-    void getAllProducts_shouldReturnPageOfProductResponses() {
-        // Arrange
-        int page = 0;
-        int size = 10;
-        String sortOrder = "asc";
-        String sortBy = "name";
-        Category category = new Category(1L, "Electronics", "Category for electronics");
-        CategoryDto categoryDto = new CategoryDto(1L, "Electronics");
-        Brand brand = new Brand(1L, "Brand A", "Brand Description");
-        BrandDto brandDto = new BrandDto(1L, "Brand A");
-        Product product = new Product(null, "Product A", "Description A", 10L, new BigDecimal(100), List.of(category), brand);
-        ProductResponse productResponse = new ProductResponse(null, "Product A", "Description A", 10L, new BigDecimal(100), List.of(categoryDto), brandDto);
-
-        Page<Product> productPage = new PageImpl<>(List.of(product));
-        Page<ProductResponse> responsePage = new PageImpl<>(List.of(productResponse));
-
-        when(productServicePort.getAllProducts(page, size, true, sortBy)).thenReturn(productPage);
-        when(productResponseMapper.toProductResponse(product)).thenReturn(productResponse);
-
-        // Act
-        ResponseEntity<Page<ProductResponse>> response = productRestController.getAllProducts(page, size, sortOrder, sortBy);
-
-        // Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(responsePage, response.getBody());
-        verify(productServicePort, times(1)).getAllProducts(page, size, true, sortBy);
-        verify(productResponseMapper, times(1)).toProductResponse(product);
-    }
-
-    @Test
     void updateProductQuantity_shouldReturnUpdatedProduct() {
         // Arrange
         Category category = new Category(1L, "Electronics", "Category for electronics");
@@ -157,5 +126,145 @@ class ProductRestControllerTest {
         // Assert
         assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
         verify(productServicePort, times(1)).deleteProduct(productName);
+    }
+
+    @Test
+    void testGetAllProductsAscending() {
+        // Arrange
+        int page = 0;
+        int size = 10;
+        String sortBy = "name";
+        boolean ascending = true;
+
+        // Simular datos de Product, Category y Brand
+        Brand brand = new Brand(1L, "Brand A", "Description A");
+        Category category = new Category(1L, "Category A", "Description A");
+        Product product = new Product(1L, "Product A", "Description A", 10L, BigDecimal.valueOf(100), List.of(category), brand);
+
+        // Simular ProductResponse
+        BrandDto brandDto = new BrandDto(1L, "Brand A");
+        CategoryDto categoryDto = new CategoryDto(1L, "Category A");
+        ProductResponse productResponse = new ProductResponse(1L, "Product A", "Description A", 10L, BigDecimal.valueOf(100), List.of(categoryDto), brandDto);
+
+        // Simular página de Product y ProductResponse
+        CustomPage<Product> pageOfProducts = new CustomPage<>();
+        pageOfProducts.setPageNumber(page);
+        pageOfProducts.setPageSize(size);
+        pageOfProducts.setTotalElements(1);
+        pageOfProducts.setTotalPages(1);
+        pageOfProducts.setContent(List.of(product));
+
+        CustomPage<ProductResponse> pageOfProductResponses = new CustomPage<>();
+        pageOfProductResponses.setPageNumber(page);
+        pageOfProductResponses.setPageSize(size);
+        pageOfProductResponses.setTotalElements(1);
+        pageOfProductResponses.setTotalPages(1);
+        pageOfProductResponses.setContent(List.of(productResponse));
+
+        // Mockear comportamiento del service y mapper
+        when(productServicePort.getAllProducts(page, size, ascending, sortBy)).thenReturn(pageOfProducts);
+        when(productResponseMapper.toPageOfProductResponse(pageOfProducts)).thenReturn(pageOfProductResponses);
+
+        // Act
+        ResponseEntity<CustomPage<ProductResponse>> response = productRestController.getAllProducts(page, size, "asc", sortBy);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(1, response.getBody().getTotalElements());
+        assertEquals(1, response.getBody().getTotalPages());
+        assertEquals(page, response.getBody().getPageNumber());
+        assertEquals(size, response.getBody().getPageSize());
+        assertEquals(1, response.getBody().getContent().size());
+
+        ProductResponse responseProduct = response.getBody().getContent().get(0);
+        assertEquals(productResponse.getProductId(), responseProduct.getProductId());
+        assertEquals(productResponse.getName(), responseProduct.getName());
+        assertEquals(productResponse.getDescription(), responseProduct.getDescription());
+        assertEquals(productResponse.getPrice(), responseProduct.getPrice());
+        assertEquals(productResponse.getQuantity(), responseProduct.getQuantity());
+
+        // Verificar categorías y marca en la respuesta
+        assertEquals(1, responseProduct.getCategories().size());
+        assertEquals(categoryDto.getCategoryId(), responseProduct.getCategories().get(0).getCategoryId());
+        assertEquals(categoryDto.getName(), responseProduct.getCategories().get(0).getName());
+
+        assertEquals(brandDto.getBrandId(), responseProduct.getBrand().getBrandId());
+        assertEquals(brandDto.getName(), responseProduct.getBrand().getName());
+
+        // Verificar que el servicio y mapper fueron llamados
+        verify(productServicePort).getAllProducts(page, size, ascending, sortBy);
+        verify(productResponseMapper).toPageOfProductResponse(pageOfProducts);
+    }
+
+    @Test
+    void testGetAllProductsDescending() {
+        // Arrange
+        int page = 0;
+        int size = 10;
+        String sortBy = "name";
+        boolean ascending = false;
+
+        // Simular datos de Product, Category y Brand
+        Brand brand = new Brand(2L, "Brand B", "Description A");
+        Category category = new Category(2L, "Category B", "Description A");
+        Product product = new Product(2L, "Product B", "Description B", 20L, BigDecimal.valueOf(200), List.of(category), brand);
+
+        // Simular ProductResponse
+        BrandDto brandDto = new BrandDto(2L, "Brand B");
+        CategoryDto categoryDto = new CategoryDto(2L, "Category B");
+        ProductResponse productResponse = new ProductResponse(2L, "Product B", "Description B", 20L, BigDecimal.valueOf(200), List.of(categoryDto), brandDto);
+
+        // Simular página de Product y ProductResponse
+        CustomPage<Product> pageOfProducts = new CustomPage<>();
+        pageOfProducts.setPageNumber(page);
+        pageOfProducts.setPageSize(size);
+        pageOfProducts.setTotalElements(1);
+        pageOfProducts.setTotalPages(1);
+        pageOfProducts.setContent(List.of(product));
+
+        CustomPage<ProductResponse> pageOfProductResponses = new CustomPage<>();
+        pageOfProductResponses.setPageNumber(page);
+        pageOfProductResponses.setPageSize(size);
+        pageOfProductResponses.setTotalElements(1);
+        pageOfProductResponses.setTotalPages(1);
+        pageOfProductResponses.setContent(List.of(productResponse));
+
+        // Mockear comportamiento del service y mapper
+        when(productServicePort.getAllProducts(page, size, ascending, sortBy)).thenReturn(pageOfProducts);
+        when(productResponseMapper.toPageOfProductResponse(pageOfProducts)).thenReturn(pageOfProductResponses);
+
+        // Act
+        ResponseEntity<CustomPage<ProductResponse>> response = productRestController.getAllProducts(page, size, "desc", sortBy);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(1, response.getBody().getTotalElements());
+        assertEquals(1, response.getBody().getTotalPages());
+        assertEquals(page, response.getBody().getPageNumber());
+        assertEquals(size, response.getBody().getPageSize());
+        assertEquals(1, response.getBody().getContent().size());
+
+        ProductResponse responseProduct = response.getBody().getContent().get(0);
+        assertEquals(productResponse.getProductId(), responseProduct.getProductId());
+        assertEquals(productResponse.getName(), responseProduct.getName());
+        assertEquals(productResponse.getDescription(), responseProduct.getDescription());
+        assertEquals(productResponse.getPrice(), responseProduct.getPrice());
+        assertEquals(productResponse.getQuantity(), responseProduct.getQuantity());
+
+        // Verificar categorías y marca en la respuesta
+        assertEquals(1, responseProduct.getCategories().size());
+        assertEquals(categoryDto.getCategoryId(), responseProduct.getCategories().get(0).getCategoryId());
+        assertEquals(categoryDto.getName(), responseProduct.getCategories().get(0).getName());
+
+        assertEquals(brandDto.getBrandId(), responseProduct.getBrand().getBrandId());
+        assertEquals(brandDto.getName(), responseProduct.getBrand().getName());
+
+        // Verificar que el servicio y mapper fueron llamados
+        verify(productServicePort).getAllProducts(page, size, ascending, sortBy);
+        verify(productResponseMapper).toPageOfProductResponse(pageOfProducts);
     }
 }
